ea9185c92003ba1c85ad937407d3f6a3572517a6
Chris Searle <chris@chrissearle.org>
2012-07-24 15:09:33 +0200

    Initial Commit

---
+++ TestRig/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestRig
+//
+//  Created by Chris Searle on 24.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestRig/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestRig
+//
+//  Created by Chris Searle on 24.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
